bf9af30c90453223159f7c2cef841b6e7fedc08a
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-08-29 16:52:24 -0400

    𝔠𝔬𝔪𝔪𝔦𝔱𝔱𝔢𝔡 𝔲𝔰𝔦𝔫𝔤 𝔞𝔯𝔠𝔞𝔫𝔢 𝔪𝔞𝔤𝔦𝔠𝔨𝔰 𝔩𝔬𝔰𝔱 𝔱𝔬 𝔱𝔥𝔦𝔰 𝔪𝔬𝔯𝔱𝔞𝔩 𝔠𝔬𝔦𝔩

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case "ADD_FRIEND":
+      if(action.friend.name !== undefined && action.friend.hometown !== undefined && action.friend.id !== undefined){
+        return {...state, friends: [...state.friends, action.friend]}
+      } else {
+        return state;
+      }
+    case "REMOVE_FRIEND":
+      return {...state, friends: state.friends.filter(element => {
+        return element.id !== action.id
+      })}
+    default:
+      return state;
+  }
 }
